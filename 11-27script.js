be689b835e3adefb1a7fdc436fbabbbfc95f31e6
Jacob Bennett <kf7ags@gmail.com>
2018-11-27 07:48:46 -0700

    initial

---
+++ 11-27script.js
@@ -0,0 +1,6 @@
+
+console.log('merry christmas');
+
+//feed carrots to reindeer, move santa down a chimney, put your face on a snowflake
+//put in a name and return naughty or nice, has to be a sound
+//imagery too
