5c270723f4db3e1e334e83f17e3cec6003fd94b3
Jacob Bennett <kf7ags@gmail.com>
2018-11-27 08:12:54 -0700

    added some functions

--- 11-27script.js
+++ 11-27script.js
@@ -4,3 +4,45 @@ console.log('merry christmas');
 //feed carrots to reindeer, move santa down a chimney, put your face on a snowflake
 //put in a name and return naughty or nice, has to be a sound
 //imagery too
+
+let theInput = document.getElementsByClassName("enterName")[0];
+let niceList = document.getElementsByClassName("niceList")[0];
+let naughtyList = document.getElementsByClassName("naughtyList")[0];
+
+function naughtyNice(name){
+    if (name.slice(0,2) === "C:\\"){
+        return "nice";
+    }
+    else if(name.slice(0,2)=== "htt"){
+        return "naughty";
+    }
+    else if(name.length === 4){
+        return "naughty";
+    }
+    else{
+        return "nice";
+    }
+}
+
+function changeDOM(){
+    let entered = theInput.value;
+    theInput.value = "";
+    let alignment = naughtyNice(entered);
+    if (alignment === "nice"){
+        if (entered.slice(0,2) === "C:\\"){
+            niceList.innerHTML += `<img src="${entered}" alt="the users picture">`;
+        }
+        else{
+            niceList.innerHTML += `<p>${entered}</p>`;
+        }
+    }
+    else if(alignment === "naughty"){
+        if (entered.slice(0,2)=== "htt"){
+            naughtyList.innterHTML += `<img src="${entered}" alt="an image from the web">`;
+        }
+        else{
+            naughtyList.innterHTML += `<p>${entered}</p>`;
+        }
+    }
+
+}
